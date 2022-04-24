62a78e48a5b927e04c7ae2e9d7cd92b38643268a
bartoszlomiej <jet.samb@gmail.com>
2022-04-25 00:46:01 +0200

    Working version of the linear SVM regression

--- svm.py
+++ svm.py
@@ -1,16 +1,15 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.model_selection import train_test_split
-from sklearn.svm import SVR
-from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.svm import LinearSVR
+from sklearn.metrics import mean_absolute_error, mean_squared_error, accuracy_score, f1_score
 import timeit
 import csv
 import datetime
 
-intForRandomState = 42              #Variable to manuipulate the simulation
+intForRandomState = 42           #Variable to manuipulate the simulation
 tSize = 0.25                        #Testing set is 25%, training set is 75% of dataset 
 
 #To display the whole table in console
@@ -39,32 +38,27 @@ metrics = np.array(dataset)
 
 #Divide dataset into training and testing set, we can change random_state to change the shuffling of the data
 train_metrics, test_metrics, train_toPredict, test_toPredict = train_test_split(metrics, toPredict, test_size = tSize, random_state = intForRandomState)
-'''
-sc_X = StandardScaler()
-sc_y = StandardScaler()
-X = sc_X.fit_transform(train_metrics)
-y = sc_y.fit_transform(train_toPredict)
-'''
+
 X = train_metrics
+X_test_norm = test_metrics
+
+scaler = StandardScaler().fit(train_metrics)
+X = scaler.transform(train_metrics)
+X_test_norm = scaler.transform(test_metrics)
+'''
+norm = MinMaxScaler().fit(train_metrics)
+X = norm.transform(train_metrics)
+X_test_norm = norm.transform(test_metrics)
+'''
 Y = train_toPredict
-regr = SVR(kernel='rbf')
+regr = LinearSVR(max_iter=2000, C=100)
 start = timeit.default_timer()
 regr.fit(X, Y)
 stop = timeit.default_timer()
 training_time = "{0:0.3f}".format(stop - start)
 print('Time to train the model: ', training_time, 's')
-predictions = regr.predict(test_metrics)
-'''
-regr = make_pipeline(StandardScaler(), LinearSVR(random_state=100, tol=1e-5))
+predictions = regr.predict(X_test_norm)
 
-start = timeit.default_timer()
-# Train the model 
-regr.fit(train_metrics, train_toPredict)
-stop = timeit.default_timer()
-training_time = "{0:0.3f}".format(stop - start)
-print('Time to train the model: ', training_time, 's')
-predictions = regr.predict(test_metrics)
-'''
 #Format data in table to 0.3f format | show all entries in arrays
 np.set_printoptions(threshold=np.inf)
 np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
@@ -84,6 +78,15 @@ with open(filename,"w+") as my_csv:
 size = test_metrics[:,4]
 plt.scatter(size, test_toPredict, color = 'red', s=0.1)
 plt.scatter(size, predictions, color = 'green', s=0.1)
+plt.title('SVM Linear Regression')
+plt.xlabel('Sqft_living')
+plt.ylabel('Price')
+plt.legend(['Original value', 'Prediction'])
+plt.show()
+
+#Graph2 - Price vs Sqft_living, less data 
+plt.scatter(size[0:50], test_toPredict[0:50], color = 'red', s=2)
+plt.scatter(size[0:50], predictions[0:50], color = 'green', s=2)
 plt.title('SVM Linear Regression')
 plt.xlabel('Sqft_living')
 plt.ylabel('Price')
